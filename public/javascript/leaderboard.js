e4e15a372b08a26e98010eb913dd6d8bcdee6f39
Bk49 <ericngyongwei@gmail.com>
2021-11-19 18:20:39 +0800

    Change common leaderboard front end rendering

--- public/javascript/leaderboard.js
+++ public/javascript/leaderboard.js
@@ -1,48 +1,157 @@
+const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
 /* EVENT LISTENER */
 $(document).ready(function () {
-    $(".header").load("topbar.html", function(){
-        document.getElementById("name").innerHTML = getName();
-    });
-    getLeaderboard();
+    getLeaderboard("Primary", 1, 1);
 });
 
-$(document).on("change", ".filter-select", function() {
-    getLeaderboard(this.value)
+// For changing of secondary and primary school lb
+$(document).on("click", "#change-level", function () {
+    const level = $("#change-level").text().split(" ")[3];
+    $("#change-level").html(
+        `<i class="fas fa-sync-alt"></i> Change to ${
+            level === "Secondary" || level === "" ? "Primary" : "Secondary"
+        } School`
+    );
+    getLeaderboard(level ? level : "Secondary", "1");
+});
+
+// For changing of dropdown: Type
+const onChangeType = (type = 1) => {
+    const level = $("#change-level").text().split(" ")[3];
+    const filterTxt = $("#type-button").text().split(" ")[0];
+    const filter = filterTxt === "Global" ? 1 : filterTxt === "School" ? 2 : 3;
+    getLeaderboard(level, type, filter);
+    $("#type-content").css("visibility", "hidden");
+
+    const icon = '<i class="fas fa-angle-down right"></i>';
+    $("#type-button").html(
+        type === 1
+            ? `Average Score ${icon}`
+            : type === 2
+            ? `Average Time Taken ${icon}`
+            : `Quizzes Attempted ${icon}`
+    );
+};
+
+// For changing of dropdown: Scope
+const onChangeFilter = (filter = 1) => {
+    const level = $("#change-level").text().split(" ")[3];
+    const typeAry = $("#type-button").text().split(" ");
+    typeAry.pop();
+    const typeTxt = typeAry.join(" ");
+    const type =
+        typeTxt === "Average Score"
+            ? 1
+            : typeTxt === "Average Time Taken"
+            ? 2
+            : 3;
+    getLeaderboard(level, type, filter);
+    $("#filter-content").css("visibility", "hidden");
+
+    const icon = '<i class="fas fa-angle-down right"></i>';
+    $("#filter-button").html(
+        filter === 1
+            ? `Global ${icon}`
+            : filter === 2
+            ? `School ${icon}`
+            : `Level ${icon}`
+    );
+};
+
+// Handling dropdown-content display and hiding
+$(document).on("click", "#type-button", () => {
+    $("#filter-content").css("visibility", "hidden");
+    const obj = $("#type-content");
+    obj.css("visibility") === "hidden"
+        ? obj.css("visibility", "visible")
+        : obj.css("visibility", "hidden");
+});
+
+$(document).on("click", "#filter-button", () => {
+    $("#type-content").css("visibility", "hidden");
+    const obj = $("#filter-content");
+    obj.css("visibility") === "hidden"
+        ? obj.css("visibility", "visible")
+        : obj.css("visibility", "hidden");
 });
 
 /* API CALLS */
-function getLeaderboard(scope = "") {
+function getLeaderboard(level = "Primary", type, filter) {
     $.ajax({
-        url: '/quiz/leaderboard?scope='+scope,
-        method: 'POST',
-        dataType: 'JSON',
+        url: `/quiz/leaderboard?sort=${type}&scope=${level}&filter=${filter}`,
+        method: "POST",
+        dataType: "JSON",
+        headers: { user: JSON.stringify(userInfo) },
         success: function (data, textStatus, xhr) {
-            displayLeaderboard(data);
+            $("#type-button").css("visibility", "hidden");
+            $(".type-dropdown").css("visibility", "hidden");
+            $("#filter-button").css("visibility", "hidden");
+            displayLeaderboard(data, parseInt(type));
         },
         error: function (xhr, textStatus, errorThrown) {
             console.log(errorThrown);
-        }
+        },
     });
 }
 
 /* DISPLAY DATA */
-function displayLeaderboard(data) {
+function displayLeaderboard(data, type) {
     let leaderboard = document.querySelector("#leaderboard");
     let top3 = document.querySelector("#top-3");
-    // let noresult = $("#noresult");
 
-    // reset ui
     leaderboard.innerHTML = "";
     top3.innerHTML = "";
-    // $("#noresult").hide();
     document.getElementById("noresult").style.display = "none";
 
     var ordinal = "";
-    let html = "";
 
     if (data.length >= 1) {
+        $("#type-button").css("visibility", "visible");
+        $(".type-dropdown").css("visibility", "visible");
+        $("#filter-button").css("visibility", "visible");
+
+        $("#leaderboard").prepend(`
+        <table class="table">
+            <thead>
+                <tr class="heading">
+                    <th class="col-1 center">Rank</th>
+                    <th class="col-3">Name</th>
+                    <th class="col-4">School</th>
+                    <th class="col-2 center">Level</th>
+                    <th class="col-2 center">${
+                        type === 1
+                            ? "Avg Score"
+                            : type === 2
+                            ? "Avg Time Taken"
+                            : "Quiz Attempted"
+                    }</th>
+                </tr>
+            </thead>
+        <tbody id="lb-tb">
+    `);
+
         for (var i = 0; i < data.length; i++) {
-            const { _id, average_score, num_of_quiz, average_time_taken, first_name, last_name, school, grade } = data[i];
+            const {
+                _id,
+                average_score,
+                num_of_quiz,
+                average_time_taken,
+                first_name,
+                last_name,
+                school,
+                grade,
+            } = data[i];
+
+            const school_proper = school
+                .split(" ")
+                .map((w) => w[0].toUpperCase() + w.substr(1).toLowerCase())
+                .join(" ");
+
+            const grade_formatted =
+                grade > 6 ? `Secondary ${grade - 6}` : `Primary ${grade}`;
+
+            const short_grade = grade > 6 ? `S${grade - 6}` : `P${grade}`;
 
             // assign ordinal (st, th, nd) to number
             if ((i + 1) % 10 == 1 && i / 10 != 1) ordinal = "st";
@@ -51,56 +160,61 @@ function displayLeaderboard(data) {
             else ordinal = "th";
 
             // if leaderboard has at least 3 ppl
-            if ((i >= 0 && i <= 2) && data.length >= 3) {
+            if (i >= 0 && i <= 2 && data.length >= 3) {
                 top3.innerHTML += `
-                    <div class="top-rank">
+                    <div class="top-rank ${
+                        i == 0 ? "first" : i == 1 ? "second" : "third"
+                    }">
                         <span class="top-rank-num">${i + 1}${ordinal}</span>
-                        <i class="fas fa-trophy"></i>
+                        <img class="pfp" src="./images/testing.jpg"/>
                         <span class="top-name">${first_name} ${last_name}</span>
-                        <span class="top-grade">Primary ${grade}</span>
-                        <span class="top-school">${school}</span>
+                        <span class="top-grade">${grade_formatted}</span>
+                        <span class="top-school">${school_proper}</span>
                         <div class="stats">
-                            <div class="top-avg-score"><strong>Avg Score</strong><br> ${average_score.toFixed(1)}</div>
-                            <div class="top-num-of-quiz"><strong>Quizzes Done</strong><br> ${num_of_quiz}</div>
-                            <div class="top-avg-time"><strong>Avg Time</strong><br> ${average_time_taken.toFixed(2)}</div>
-                        </div>
+                            <span class="top-avg-score-header">${
+                                type === 1
+                                    ? "Avg Score"
+                                    : type === 2
+                                    ? "Avg Time Taken"
+                                    : "Quiz Attempted"
+                            }</span><br>
+                            <span class="top-avg-score">${
+                                type === 1
+                                    ? average_score.toFixed(0)
+                                    : type === 2
+                                    ? average_time_taken.toFixed(2) + " min(s)"
+                                    : num_of_quiz
+                            }</span>
+                        <div />
                     </div>
                 `;
-            }
-            else {
-                html += `
-                    <div class="student-row row d-flex align-items-center">
-                        <span class="col-4 student-rank">
-                            <span class="rank">${i + 1}${ordinal}</span>
-                            <span class="name"> ${first_name} ${last_name}</span>
-                        </span>
-                        <span class="grade col-1">${grade}</span>
-                        <span class="school col-2">${school}</span>
-                        <span class="average-score col-2">${average_score.toFixed(1)}</span>
-                        <span class="num-of-quiz col-1">${num_of_quiz}</span>
-                        <span class="average-time-taken col-2">${average_time_taken.toFixed(2)}</span>
-                    </div>
-                `
-            }
-        }
-
-        leaderboard.innerHTML = html;
-
-        // add table heading of table not empty
-        if (html != "") {
-            $("#leaderboard").prepend(`
-                <div class="heading row">
-                    <span class="name-label col-4">Name</span>
-                    <span class="grade col-1">Grade</span>
-                    <span class="school col-2">School</span>
-                    <span class="average-score col-2">Average Score</span>
-                    <span class="num-of-quiz col-1">Quizzes Done</span>
-                    <span class="average-time-taken col-2">Average Time</span>
-                </div>
+            } else if (i <= 9 || _id === userInfo._id) {
+                $("#lb-tb").append(`
+                    <tr class="student-row">
+                        <td class="center">${i + 1}</td>
+                        <td>${`${first_name} ${last_name}`}</td>
+                        <td>${school_proper}</td>
+                        <td class="center">${short_grade}</td>
+                        <td class="center">${
+                            type === 1
+                                ? average_score.toFixed(0)
+                                : type === 2
+                                ? average_time_taken.toFixed(2) + " min(s)"
+                                : num_of_quiz
+                        }</td>
+                    </tr>
                 `);
+            }
         }
-    }
-    else {
+
+        $("#leaderboard").append(`
+                </tbody>
+            </table>
+        `);
+
+        // This is to render the user's placement in the leaderboard
+        html += ""; // To be added in once the group leaderboard API is recreated
+    } else {
         document.getElementById("noresult").style.display = "flex";
     }
 }
