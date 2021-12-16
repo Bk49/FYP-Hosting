731da613581464be2ce4ef70525e6ce391bb0d25
zheyii-uwu <64114969+zheyii-uwu@users.noreply.github.com>
2021-12-16 20:58:35 +0800

    Reactnative Login

--- mobile-app/components/pages/individual-pages/Home.js
+++ mobile-app/components/pages/individual-pages/Home.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Button,View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/stack';
 
-export default Home = () => {
+export default Home = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <Text>Home Page!</Text>
+            <View style={styles.buttonContainer}>
+                <Button style={styles.button} title="Login" onPress={() => navigation.push('Login')}/>
+                <Button style={styles.button} title="Sign Up" onPress={() => navigation.navigate('SignUp')}/>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    buttonContainer: {
+        width: 500,
+        height: 300,
+    },
 })
--- mobile-app/components/pages/individual-pages/SignUp.js
+++ mobile-app/components/pages/individual-pages/SignUp.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { View } from "react-native";
+import { Button, View, Text, StyleSheet } from "react-native";
 
 export default SignUp = () => {
     return (
-        <View></View>
+        <View style={styles.container}>
+            <Text style={styles.text}>SignUp Page</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#003744',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color:'white',
+    }
+})
