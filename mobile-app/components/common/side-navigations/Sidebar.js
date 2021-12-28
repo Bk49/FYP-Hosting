b1d8932e0900226d251605a3a34786cf493b0fb4
Bk49 <ericngyongwei@gmail.com>
2021-12-28 14:14:12 +0800

    Sidebar modifications for dynamic styling

--- mobile-app/components/common/side-navigations/Sidebar.js
+++ mobile-app/components/common/side-navigations/Sidebar.js
@@ -1,94 +1,153 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Image, Header, TouchableOpacity } from 'react-native';
-import { Entypo, FontAwesome5, Feather, Ionicons } from '@expo/vector-icons'; 
-import { useNavigate } from "react-router-native";
-import SidebarItem from './SidebarItem';
-import client from "../../../axios/clientConfig"
-import path from "../../../axios/paths";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { Entypo, FontAwesome5, Feather, Ionicons } from "@expo/vector-icons";
+import SidebarItem from "./SidebarItem";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import login from '../../../axios/user-api/login';
 
+const icons = {
+    group: <FontAwesome5 name="users" size={24} color="black" />,
+    leaderboard: <FontAwesome5 name="award" size={24} color="black" />,
+    learningrss: <FontAwesome5 name="newspaper" size={24} color="black" />,
+    overview: <Entypo name="home" size={24} color="black" />,
+    quiz: <FontAwesome5 name="atom" size={24} color="black" />,
+    assignment: <Feather name="clipboard" size={24} color="black" />,
+    statistics: <FontAwesome5 name="chart-bar" size={24} color="black" />,
+    control: <Ionicons name="settings" size={24} color="black" />,
+};
 
-export default Sidebar = ({currentPage}) => {
+const directory = {
+    overview: "/Overview",
+    quiz: "/Quiz",
+    assignment: "/Assignment",
+    group: "/",
+    statistics: "/Stats",
+    leaderboard: "/Leaderboard",
+    learningrss: "/LearningResourcesLevel",
+    control: "/",
+};
 
-    const navigate = useNavigate();
-    const [sideBar, setSideBar] = useState();
-    const [data, setData] = useState();
-    // 1. get the role of the user
-    // 2. render sidebar based on user's role
-    const getUserData = async () => {
-        const data = await AsyncStorage.getItem("userInfo");
-        return JSON.parse(data)
-    };  
+export default Sidebar = ({ currentPage }) => {
+    const [userRole, setUserRole] = useState();
 
-    useEffect(() => {
-        console.log(currentPage === "Overview")
-        getUserData()
-        .then((data) => {
-            setData(data);
-            if (data.role == "teacher" || data.role == "parent") {
-                setSideBar(
-                    <View>
-                        <SidebarItem isHere={(currentPage === "Groups")} icon={<FontAwesome5 name="users" size={24} color="black" />} text={" My Groups"}></SidebarItem>
-                        <SidebarItem isHere={(currentPage === "Leaderboard")} icon={<FontAwesome5 name="award" size={24} color="black" />} text=" Leaderboard"></SidebarItem>
-                        <SidebarItem isHere={(currentPage === "Learning Resources")} icon={<FontAwesome5 name="newspaper" size={24} color="black" />} text=" Learning Resources"></SidebarItem>
-                    </View>
-                )
-            }
-            else if (data.role == "student") {
-                setSideBar(
-                    <View>
-                        <SidebarItem isHere={currentPage} icon={<Entypo name="home" size={24} color="black" />} text={" Overview"}></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<FontAwesome5 name="atom" size={24} color="black" />} text={" Quiz"}></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<Feather name="clipboard" size={24} color="black" />} text={" Assignments"}></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<FontAwesome5 name="users" size={24} color="black" />} text={" My Groups"}></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<FontAwesome5 name="chart-bar" size={24} color="black" />} text=" My Statistics"></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<FontAwesome5 name="award" size={24} color="black" />} text=" Leaderboard"></SidebarItem>
-                        <SidebarItem isHere={currentPage} icon={<FontAwesome5 name="newspaper" size={24} color="black" />} text=" Learning Resources"></SidebarItem>
-                    </View>
-                )
-            }
-            else if (data.role == "admin") {
-                setSideBar(
-                    <View>
-                        <SidebarItem isHere={(currentPage === "Quiz Control Panel")} icon={<Ionicons name="settings" size={24} color="black" />} text={" Quiz Control Panel"}></SidebarItem>
-                        <SidebarItem isHere={(currentPage === "Leaderboard")} icon={<FontAwesome5 name="award" size={24} color="black" />} text={" Leaderboard"}></SidebarItem>
-                    </View>
-                )
-            }
-        });   
-    }, [])
+    useEffect(async () => {
+        const userInfo = await AsyncStorage.getItem("userInfo");
+        const { role } = JSON.parse(userInfo);
+        setUserRole(role);
+    }, []);
 
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require('../../../assets/Psleonline_logo_transparent.png')}></Image>    
-            {sideBar}
+            <Image
+                style={styles.image}
+                source={require("../../../assets/Psleonline_logo_transparent.png")}
+            ></Image>
+            {userRole === "teacher" || userRole === "parent" ? (
+                <View>
+                    <SidebarItem
+                        icon={icons.group}
+                        label="My Groups"
+                        isActive={currentPage === "My Groups"}
+                        directory={directory.group}
+                    />
+                    <SidebarItem
+                        icon={icons.leaderboard}
+                        label="Leaderboard"
+                        isActive={currentPage === "Leaderboard"}
+                        directory={directory.leaderboard}
+                    />
+                    <SidebarItem
+                        icon={icons.learningrss}
+                        label="Learning Resources"
+                        isActive={currentPage === "Learning Resources"}
+                        directory={directory.learningrss}
+                    />
+                </View>
+            ) : userRole === "student" ? (
+                <View>
+                    <SidebarItem
+                        icon={icons.overview}
+                        label="Overview"
+                        isActive={currentPage === "Overview"}
+                        directory={directory.overview}
+                    />
+                    <SidebarItem
+                        icon={icons.quiz}
+                        label="Quiz"
+                        isActive={currentPage === "Quiz"}
+                        directory={directory.quiz}
+                    />
+                    <SidebarItem
+                        icon={icons.assignment}
+                        label="Assignments"
+                        isActive={currentPage === "Assignment"}
+                        directory={directory.assignment}
+                    />
+                    <SidebarItem
+                        icon={icons.group}
+                        label="My Groups"
+                        isActive={currentPage === "My Groups"}
+                        directory={directory.group}
+                    />
+                    <SidebarItem
+                        icon={icons.statistics}
+                        label="My Statistics"
+                        isActive={currentPage === "My Statistics"}
+                        directory={directory.statistics}
+                    />
+                    <SidebarItem
+                        icon={icons.leaderboard}
+                        label="Leaderboard"
+                        isActive={currentPage === "Leaderboard"}
+                        directory={directory.leaderboard}
+                    />
+                    <SidebarItem
+                        icon={icons.learningrss}
+                        label="Learning Resources"
+                        isActive={currentPage === "Learning Resources"}
+                        directory={directory.learningrss}
+                    />
+                </View>
+            ) : userRole === "admin" ? (
+                <View>
+                    <SidebarItem
+                        icon={icons.control}
+                        label="Quiz Control Panel"
+                        isActive={currentPage === "Quiz Control Panel"}
+                        directory={directory.control}
+                    />
+                    <SidebarItem
+                        icon={icons.leaderboard}
+                        label="Leaderboard"
+                        isActive={currentPage === "Leaderboard"}
+                        directory={directory.leaderboard}
+                    />
+                </View>
+            ) : (
+                <View></View>
+            )}
         </View>
     );
-}
-
+};
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#E1F1FF',
-        height: '100%',
-        width: '25%',
+        backgroundColor: "#E1F1FF",
+        height: "100%",
+        width: "25%",
         paddingTop: 10,
-        
     },
     buttons: {
-        paddingVertical: 5
+        paddingVertical: 5,
     },
-    buttonText: {
-        // fontFamily: 'Poppins_400Regular',
+    buttonlabel: {
+        fontFamily: "Poppins",
         fontSize: 25,
         paddingTop: 10,
         paddingLeft: 10,
-        paddingRight: 10
+        paddingRight: 10,
     },
     image: {
-        width: '100%',
-        height: '10%',
-    }
-})
-
+        width: "100%",
+        height: "10%",
+    },
+});
--- mobile-app/components/common/side-navigations/SidebarItem.js
+++ mobile-app/components/common/side-navigations/SidebarItem.js
@@ -1,64 +1,37 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Header, TouchableOpacity, Pressable, Touchable } from 'react-native';
-import { Entypo, FontAwesome5, Feather } from '@expo/vector-icons';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useNavigate } from "react-router-native";
 
-export default SidebarItem = (item, { isHere }) => {
-    const navigate = useNavigate();
-    const [checked, toggleChecked] = useState(false);
-    const [color, setColor] = useState(item.text.trim() === isHere ? '#FFFFFF' : '#E1F1FF');
-    console.log(item.text.trim() + item.text.trim() === isHere);
-    const navigatePage = () => {
-        if (item.text.includes("Overview")) {
-            navigate("/Overview");
-        }
-        else if (item.text.includes("Quiz")) {
-            navigate("/Quiz");
-        }
-        else if (item.text.includes("Assignments")) {
-            navigate("/Assignment")
-        }
-        else if (item.text.includes("My Groups")) {
-            navigate("/");
-        }
-        else if (item.text.includes("My Statistics")) {
-            navigate("/Stats")
-        }
-        else if (item.text.includes("Leaderboard")) {
-            navigate("/Leaderboard");
-        }
-        else if (item.text.includes("Learning Resources")) {
-            navigate("/LearningResourcesLevel");
-        }
-        else if (item.text.includes("Quiz Control Panel")) {
-            navigate("/")
-        }
-    }
+export default SidebarItem = ({ label, icon, isActive, directory }) => {
+    const navigate = useNavigate()
 
     return (
-        item.text.trim() === isHere ? (
-            <View>
-                <TouchableOpacity style={{ paddingVertical: 5, backgroundColor: '#E1F1FF' }} checked={checked} onPress={() => navigatePage()}>
-                    <Text style={styles.buttonText}>{item.icon}{item.text}</Text>
-                </TouchableOpacity>
-            </View>)
-            : (
-                <View>
-                    <TouchableOpacity style={{ paddingVertical: 5, backgroundColor: '#FFFFFF' }} checked={checked} onPress={() => navigatePage()}>
-                        <Text style={styles.buttonText}>{item.icon}{item.text}</Text>
-                    </TouchableOpacity>
-                </View>)
-
-    )
-}
-
+        <View>
+            <TouchableOpacity
+                style={isActive ? styles.activeStyle : styles.inactiveStyle}
+                onPress={() => navigate(directory)}
+            >
+                <Text style={styles.buttonText}>
+                    {icon}
+                    {` ${label}`}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     buttonText: {
-        // fontFamily: 'Poppins_400Regular',
         fontSize: 25,
         paddingTop: 10,
         marginLeft: 20,
         marginRight: 20,
     },
-})
+    inactiveStyle: {
+        paddingVertical: 5,
+    },
+    activeStyle: {
+        paddingVertical: 5,
+        backgroundColor: "white",
+    },
+});
