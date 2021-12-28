c098d780aebfeb0945e1975de7d76e71c96c8d60
Bk49 <ericngyongwei@gmail.com>
2021-12-28 14:13:19 +0800

    Fix merge conflicts on paths

--- mobile-app/axios/paths.js
+++ mobile-app/axios/paths.js
@@ -1,19 +1,13 @@
 const userPath = "/user";
 const quizPath = "/quiz";
-<<<<<<< Updated upstream
 const assignmentPath = "/assignment";
 const notificationPath = "/notification";
-=======
 const levelPath = "/level";
->>>>>>> Stashed changes
 
 export default {
     userPath: userPath,
     quizPath: quizPath,
-<<<<<<< Updated upstream
     assignmentPath: assignmentPath,
     notificationPath: notificationPath,
-=======
     levelPath: levelPath
->>>>>>> Stashed changes
 };
