8418fe6d9d03894bfc878b7e8da7073bab2fcdd3
Bk49 <ericngyongwei@gmail.com>
2022-01-07 14:31:46 +0800

    Ignore logging for dataUrl when it is too long

--- controller/app.js
+++ controller/app.js
@@ -1,6 +1,6 @@
 var connection;
-const isDevelopment = process.env.NODE_ENV === 'development';
-const path = require('path');
+const isDevelopment = process.env.NODE_ENV === "development";
+const path = require("path");
 
 const mongoose = require("mongoose");
 const express = require("express");
@@ -12,56 +12,72 @@ const skillController = require("./skillController.js");
 const quizController = require("./quizController.js");
 const questionController = require("./questionController.js");
 const groupController = require("./groupController.js");
-const { router, postSocket, updatePostSocket, deletePostSocket } = require("./postController.js");
+const {
+    router,
+    postSocket,
+    updatePostSocket,
+    deletePostSocket,
+} = require("./postController.js");
 const postController = router;
-const assignmentController = require('./assignmentController');
-const notificationController = require('./notificationController');
-const gameController = require('./gameController.js');
-const qnaController = require('./qnaController.js');
-
+const assignmentController = require("./assignmentController");
+const notificationController = require("./notificationController");
+const gameController = require("./gameController.js");
+const qnaController = require("./qnaController.js");
+const shareController = require("./shareController.js");
 
 // const email = require("../email/email");
 const app = express();
 
 // cookie parser middleware
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 // cors middleware
-const cors = require('cors');
-app.options('*', cors());
-app.use(cors({
-    credentials: true,
-    origin: true,
-    methods: ['GET', 'PUT', 'POST', "DELETE"],
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-}));
-
+const cors = require("cors");
+app.options("*", cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: true,
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        allowedHeaders:
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    })
+);
 
 // json middleware
-app.use(express.json({
-    verify: (_req, res, buf, _encoding) => {
-        try {
-            JSON.parse(buf);
-        } catch (e) {
-            // returns error if body is not json
-            return res.status(400).send({ error: "Invalid Request Body", code: "INVALID_JSON_BODY" });
-        }
-    }
-}));
-app.use(express.urlencoded({ extended: false }));
-
+app.use(
+    express.json({
+        verify: (_req, res, buf, _encoding) => {
+            try {
+                JSON.parse(buf);
+            } catch (e) {
+                // returns error if body is not json
+                return res
+                    .status(400)
+                    .send({
+                        error: "Invalid Request Body",
+                        code: "INVALID_JSON_BODY",
+                    });
+            }
+        },
+        limit: "50mb",
+    })
+);
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 /* host web pages */
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 /* Connect to MondoBD Instance */
-const localurl = isDevelopment ? ("mongodb://localhost:27017/" + process.env.DATABASE_CLUSTER) : process.env.CONNECTION_STRING;
+const localurl = isDevelopment
+    ? "mongodb://localhost:27017/" + process.env.DATABASE_CLUSTER
+    : process.env.CONNECTION_STRING;
 const options = {
     useNewUrlParser: true, // avoid deprecation when connecting
     useUnifiedTopology: true,
     useCreateIndex: true, // avoid deprecation for index when defining schema
-    useFindAndModify: false // avoid deprecation for findOneAndUpdate()
+    useFindAndModify: false, // avoid deprecation for findOneAndUpdate()
 };
 
 // run database connect
@@ -76,20 +92,26 @@ const options = {
 })();
 
 // handle any transaction error to mongodb
-mongoose.connection.on('error', err => {
+mongoose.connection.on("error", (err) => {
     console.error("\x1b[31mDatabase error:", err, "\x1b[0m");
 });
-
 
 // logging middleware
 app.use((req, _res, next) => {
     // go to for console.log font color
     // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-    console.log("\x1b[43m\x1b[30m", req.method, "\x1b[0m\x1b[1m", req.path, "\x1b[0m");
+    console.log(
+        "\x1b[43m\x1b[30m",
+        req.method,
+        "\x1b[0m\x1b[1m",
+        req.path,
+        "\x1b[0m"
+    );
     if (Object.keys(req.query).length !== 0)
         console.log("\x1b[34mquery:\x1b[0m", req.query);
     if (Object.keys(req.body).length !== 0)
-        console.log("\x1b[34mbody:\x1b[0m", req.body);
+        if(req.body.dataUrl && req.body.dataUrl.length >= 9999) console.log("dataUrl is very long, skip logging")
+        else console.log("\x1b[34mbody:\x1b[0m", req.body);
     next();
 });
 
@@ -111,7 +133,7 @@ app.use("/question", questionController);
 // group routes
 app.use("/group", groupController);
 app.use("/post", postController);
-app.use('/assignment', assignmentController);
+app.use("/assignment", assignmentController);
 
 // notification routes
 app.use("/notification", notificationController);
@@ -120,18 +142,23 @@ app.use("/game", gameController);
 // QnA routes
 app.use("/qna", qnaController);
 
+// Share routes
+app.use("/share", shareController);
+
 // uncaught error handling
 app.use((_error, _req, res, _next) => {
     // Any request to this server will get here, and will send an HTTP
-    res.status(500).send({ error: "An error occured", code: "UNEXPECTED_ERROR" });
+    console.log(_error);
+    res.status(500).send({
+        error: "An error occured",
+        code: "UNEXPECTED_ERROR",
+    });
 });
-
 
 // to handle paths that do not exist
 app.all("*", (_req, res) => {
-    res.status(404).sendFile(path.resolve("public/404.html"))
+    res.status(404).sendFile(path.resolve("public/404.html"));
 });
-
 
 /**
  * WEBSOCKET
@@ -142,102 +169,115 @@ let client = {};
 const socketListener = (server) => {
     var socket = new WebSocket.Server({ server });
 
-    socket.on("connection", connection = (ws) => {
-        console.log("A user is connected");
+    socket.on(
+        "connection",
+        (connection = (ws) => {
+            console.log("A user is connected");
 
-        ws.on("message", async message => {
-            let msg = JSON.parse(message);
-            console.log("Socket Request", msg);
-            switch (msg.action) {
-                case "message": {
-                    try {
-                        const new_id = await postSocket(msg);
-                        
-                        if (new_id) {
-                            msg.post._id = new_id;
-                            Object.entries(client[msg.group_id]).forEach(([ahh, sock]) => {
-                                sock.send(JSON.stringify(msg));
-                            });
+            ws.on("message", async (message) => {
+                let msg = JSON.parse(message);
+                console.log("Socket Request", msg);
+                switch (msg.action) {
+                    case "message": {
+                        try {
+                            const new_id = await postSocket(msg);
+
+                            if (new_id) {
+                                msg.post._id = new_id;
+                                Object.entries(client[msg.group_id]).forEach(
+                                    ([ahh, sock]) => {
+                                        sock.send(JSON.stringify(msg));
+                                    }
+                                );
+                            }
+                        } catch (err) {
+                            console.log(err);
                         }
-                    } catch(err) {
-                        console.log(err)
+                        break;
                     }
-                    break;
-                }
-                case "join": {
-                    let group_id = msg.group_id;
-                    let user_id = msg.user_id;
+                    case "join": {
+                        let group_id = msg.group_id;
+                        let user_id = msg.user_id;
 
-                    // if group not exists, create one
-                    if (!client[group_id]) client[group_id] = {};
+                        // if group not exists, create one
+                        if (!client[group_id]) client[group_id] = {};
 
-                    // if same user makes another connection to the same group, the previous connection will be alerted then overwritten
-                    if (client[group_id][user_id]) {
-                        console.log("The user's connection to the group already exists");
-                        let data = {
-                            action: "duplicate member"
-                        };
-                        client[group_id][user_id].send(JSON.stringify(data));
-                    }
-                    client[group_id][user_id] = ws;
-                    break;
-                }
-                case "update message": {
-                    try {
-                        const result = await updatePostSocket(msg.postId, msg.content);
-
-                        if (result) {
-                            Object.entries(client[msg.group_id]).forEach(([ahh, sock]) => {
-                                sock.send(message);
-                            });
+                        // if same user makes another connection to the same group, the previous connection will be alerted then overwritten
+                        if (client[group_id][user_id]) {
+                            console.log(
+                                "The user's connection to the group already exists"
+                            );
+                            let data = {
+                                action: "duplicate member",
+                            };
+                            client[group_id][user_id].send(
+                                JSON.stringify(data)
+                            );
                         }
-                    } catch(err) {
-                        console.log(err)
+                        client[group_id][user_id] = ws;
+                        break;
                     }
-                    break;
-                }
-                case "delete message": {
-                    try {
-                        const result = await deletePostSocket(msg.postId);
+                    case "update message": {
+                        try {
+                            const result = await updatePostSocket(
+                                msg.postId,
+                                msg.content
+                            );
 
-                        if (result) {
-                            Object.entries(client[msg.group_id]).forEach(([ahh, sock]) => {
-                                sock.send(message);
-                            });
+                            if (result) {
+                                Object.entries(client[msg.group_id]).forEach(
+                                    ([ahh, sock]) => {
+                                        sock.send(message);
+                                    }
+                                );
+                            }
+                        } catch (err) {
+                            console.log(err);
                         }
-                    } catch(err) {
-                        console.log(err)
+                        break;
                     }
-                    break;
-                }
-                case "leave": {
-                    let group_id = msg.group_id;
-                    let user_id = msg.user_id;
-                    console.log(user_id, "leaving group");
-                    if (!client[group_id][user_id]) return; // not not in group do nothing
+                    case "delete message": {
+                        try {
+                            const result = await deletePostSocket(msg.postId);
 
-                    // if the one exiting is the last one, destroy the group
-                    if (Object.keys(client[group_id]).length === 1) delete client[group_id];
-                    
-                    else delete client[group_id][user_id];// otherwise simply leave the group
-                }
-            }
+                            if (result) {
+                                Object.entries(client[msg.group_id]).forEach(
+                                    ([ahh, sock]) => {
+                                        sock.send(message);
+                                    }
+                                );
+                            }
+                        } catch (err) {
+                            console.log(err);
+                        }
+                        break;
+                    }
+                    case "leave": {
+                        let group_id = msg.group_id;
+                        let user_id = msg.user_id;
+                        console.log(user_id, "leaving group");
+                        if (!client[group_id][user_id]) return; // not not in group do nothing
 
-        });
-        ws.on("error", error => {
-            console.log("WEBSOCKET ERROR:", error);
-        });
-        ws.on("close", () => {
-            console.log("Client Connection Closed");
+                        // if the one exiting is the last one, destroy the group
+                        if (Object.keys(client[group_id]).length === 1)
+                            delete client[group_id];
+                        else delete client[group_id][user_id]; // otherwise simply leave the group
+                    }
+                }
+            });
+            ws.on("error", (error) => {
+                console.log("WEBSOCKET ERROR:", error);
+            });
+            ws.on("close", () => {
+                console.log("Client Connection Closed");
+            });
         })
-    });
+    );
 };
 
-
 // handle unhandledrejection to prevent program from breaking
-process.on('unhandledRejection', error => {
-    console.log('\x1b[31mWARNING! unhandledRejection\x1b[0m', error);
+process.on("unhandledRejection", (error) => {
+    console.log("\x1b[31mWARNING! unhandledRejection\x1b[0m", error);
 });
-
 
 module.exports = { app, socketListener };
