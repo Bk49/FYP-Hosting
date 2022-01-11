6cabd79847630db0c04e1e63e25a1491e6c27eae
Bk49 <ericngyongwei@gmail.com>
2022-01-11 13:58:56 +0800

    Remove hostname in console.log()

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000
 // var domain = isDevelopment ? ("localhost:"+port) : process.env.DOMAIN_NAME;
     
 const server = app.listen(port, () => {
-    console.log(`Server is listening on ${hostname}:${port}` );
+    console.log(`Server is listening on port ${port}` );
 });
 
 socketListener(server);
